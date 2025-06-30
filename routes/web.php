64d7a0006c00cebe7297a1befa091f64ef208d59
jpgerber1986 <jordangerber@gmail.com>
2025-06-30 12:19:05 -0600

    ADD: Placeholders

--- routes/web.php
+++ routes/web.php
@@ -8,25 +8,42 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
+// ! OPEN ROUTES ! //
+Route::get('/', Desktop::class)->name('desktop');
+
+// * MEDICAL ROUTES * //
+// Route::prefix('medical')->name('medical.')->group(function() {})
+
+// * DRIVE ROUTES * //
+// Route::prefix('drive')->name('drive.')->group(function() {})
+
+// * WEATHER ROUTES * //
+// Route::prefix('weather')->name('weather.')->group(function() {})
+
+// * SCHOOL ROUTES * //
+// Route::prefix('school')->name('school.')->group(function() {})
+
+
+
+
+
+
+
+
+
+
+
+
 // ! GUEST ROUTES ! //
 Route::middleware('guest')->group(function () {
     Route::get('/login', Login::class)->name('login');
     Route::get('/register', Register::class)->name('register');
 });
 
-// ! OPEN ROUTES ! //
-Route::get('/', Desktop::class)->name('desktop');
-
 // ! AUTHENTICATED ROUTES ! //
 Route::middleware('auth')->group(function () {
     //
 });
-
-
-
-
-
-
 
 // ! DEBUGGING DEVELOPMENT ROUTES ! //
 if (app()->environment('local')) {
